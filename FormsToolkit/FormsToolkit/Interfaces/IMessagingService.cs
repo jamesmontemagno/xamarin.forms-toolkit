712556461e78a3f81e39ee3105173a31b6fa697c
James Montemagno <james.montemagno@gmail.com>
2016-01-02 22:54:24 -0700

    Add Messaging Service & Update Global AssemblyInfo

---
+++ FormsToolkit/FormsToolkit/Interfaces/IMessagingService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace FormsToolkit
+{
+    /// <summary>
+    /// Interface to simplify messaging center
+    /// Never have to worry about the sender or subscriber, everything is sent/received for you
+    /// </summary>
+    public interface IMessagingService
+    {
+        /// <summary>
+        /// Subscribe the specified message and callback.
+        /// </summary>
+        /// <param name="message">Message.</param>
+        /// <param name="callback">Callback.</param>
+        void Subscribe(string message, Action<IMessagingService> callback);
+
+        /// <summary>
+        /// Subscribe the specified message and callback.
+        /// </summary>
+        /// <param name="message">Message.</param>
+        /// <param name="callback">Callback.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
+        void Subscribe<T>(string message, Action<IMessagingService, T> callback);
+
+        /// <summary>
+        /// Sends the message.
+        /// </summary>
+        /// <param name="message">Message.</param>
+        void SendMessage(string message);
+
+        /// <summary>
+        /// Sends the message.
+        /// </summary>
+        /// <param name="message">Message.</param>
+        /// <param name="args">Arguments.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
+        void SendMessage<T>(string message, T args);
+
+        /// <summary>
+        /// Unsubscribe the specified message.
+        /// </summary>
+        /// <param name="message">Message.</param>
+        void Unsubscribe (string message);
+
+        /// <summary>
+        /// Unsubscribe the specified message and args.
+        /// </summary>
+        /// <param name="message">Message.</param>
+        /// <param name="args">Arguments.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
+        void Unsubscribe<T> (string message, T args);
+    }
+}
+
---
+++ FormsToolkit/FormsToolkit/Services/MessagingService.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace FormsToolkit
+{
+    /// <summary>
+    /// Messaging service implementation
+    /// </summary>
+    public class MessagingService : IMessagingService
+    {
+        /// <summary>
+        /// Subscribe the specified message and callback.
+        /// </summary>
+        /// <param name="message">Message.</param>
+        /// <param name="callback">Callback.</param>
+        public void Subscribe(string message, Action<IMessagingService> callback) =>
+            MessagingCenter.Subscribe<MessagingService>(this, message, callback);
+
+
+        /// <summary>
+        /// Subscribe the specified message and callback.
+        /// </summary>
+        /// <param name="message">Message.</param>
+        /// <param name="callback">Callback.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
+        public void Subscribe<T>(string message, Action<IMessagingService, T> callback) =>
+            MessagingCenter.Subscribe<MessagingService, T>(this, message, callback);
+
+
+        /// <summary>
+        /// Sends the message.
+        /// </summary>
+        /// <param name="message">Message.</param>
+        public void SendMessage(string message) =>
+            MessagingCenter.Send<MessagingService>(this, message);
+
+
+        /// <summary>
+        /// Sends the message.
+        /// </summary>
+        /// <param name="message">Message.</param>
+        /// <param name="args">Arguments.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
+        public void SendMessage<T>(string message, T args) =>
+            MessagingCenter.Send<MessagingService, T>(this, message, args);
+
+
+        /// <summary>
+        /// Unsubscribe the specified message.
+        /// </summary>
+        /// <param name="message">Message.</param>
+        public void Unsubscribe (string message) =>
+            MessagingCenter.Unsubscribe<MessagingService>(this, message);
+
+
+        /// <summary>
+        /// Unsubscribe the specified message and args.
+        /// </summary>
+        /// <param name="message">Message.</param>
+        /// <param name="args">Arguments.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
+        public void Unsubscribe<T> (string message, T args) =>
+            MessagingCenter.Unsubscribe<MessagingService, T>(this, message);
+
+    }
+
+    /// <summary>
+    /// Messaging service question.
+    /// </summary>
+    public class MessagingServiceQuestion
+    {
+        /// <summary>
+        /// Gets or sets the title.
+        /// </summary>
+        /// <value>The title.</value>
+        public string Title {get;set;}
+        /// <summary>
+        /// Gets or sets the question.
+        /// </summary>
+        /// <value>The question.</value>
+        public string Question { get; set; }
+        /// <summary>
+        /// Gets or sets the positive button text.
+        /// </summary>
+        /// <value>The positive.</value>
+        public string Positive { get; set; }
+
+        /// <summary>
+        /// Gets or sets the negative button text
+        /// </summary>
+        /// <value>The negative.</value>
+        public string Negative { get; set; }
+    }
+
+    /// <summary>
+    /// Messaging service alert.
+    /// </summary>
+    public class MessagingServiceAlert
+    {
+        /// <summary>
+        /// Gets or sets the title.
+        /// </summary>
+        /// <value>The title.</value>
+        public string Title {get;set;}
+        /// <summary>
+        /// Gets or sets the message.
+        /// </summary>
+        /// <value>The message.</value>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance cancel/OK text.
+        /// </summary>
+        /// <value><c>true</c> if this instance cancel; otherwise, <c>false</c>.</value>
+        public string Cancel { get; set; }
+    }
+}
+
